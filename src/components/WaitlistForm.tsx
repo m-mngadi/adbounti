e2752c612b7c3eb6c4d76965c34963d6be9fc41d
Mziwenhlanhla Mngadi <mngadimziwenhlanhla@gmail.com>
2024-04-25 20:46:41 +0200

    Added skeleton for waitlist form pending status

--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 import { addToWaitlist } from "@/actions/addToWaitlist";
 import clsx from "clsx";
 import {
@@ -17,6 +17,7 @@ const WaitlistForm = () => {
     email: "",
     message: "",
   });
+  const { pending } = useFormStatus();
   const [emailAddress, setEmailAddress] = useState(formState!.email);
 
   const { toast } = useToast();
@@ -37,49 +38,62 @@ const WaitlistForm = () => {
       <span className="block text-base font-bold text-slate-700 mb-2">
         Join our waitlist
       </span>
-      <div className="block md:flex w-full md:gap-4">
-        <div className="flex-grow min-w-64">
-          <input
-            id="wl_emailAddress"
-            className="border rounded-lg h-12 w-full px-4 py-2"
-            type="email"
-            name="emailAddress"
-            placeholder="Enter your email"
-            value={emailAddress}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              setEmailAddress(e.target.value);
-            }}
-          />
-        </div>
-        <div className="flex relative items-center justify-center min-w-44 mt-3 md:mt-0">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button
-                  className={clsx(
-                    "h-12 w-full px-4 py-2 md:size-full  rounded-lg font-bold",
-                    {
-                      "bg-gradient-to-r from-rose-500 to-orange-500 text-white hover:bg-gradient-to-r hover:from-rose-600 hover:to-orange-600":
-                        emailAddress !== "",
-                    },
-                    {
-                      "bg-gray-200 text-gray-800 cursor-not-allowed":
-                        emailAddress === "",
-                    }
-                  )}
-                  disabled={emailAddress === ""}
-                  type="submit"
+      {pending === false ? (
+        <div className="block md:flex w-full md:gap-4">
+          <div className="flex-grow min-w-64">
+            <input
+              id="wl_emailAddress"
+              className="border rounded-lg h-12 w-full px-4 py-2"
+              type="email"
+              name="emailAddress"
+              placeholder="Enter your email"
+              value={emailAddress}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setEmailAddress(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex relative items-center justify-center min-w-44 mt-3 md:mt-0">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    className={clsx(
+                      "h-12 w-full px-4 py-2 md:size-full  rounded-lg font-bold",
+                      {
+                        "bg-gradient-to-r from-rose-500 to-orange-500 text-white hover:bg-gradient-to-r hover:from-rose-600 hover:to-orange-600":
+                          emailAddress !== "",
+                      },
+                      {
+                        "bg-gray-200 text-gray-800 cursor-not-allowed":
+                          emailAddress === "",
+                      }
+                    )}
+                    disabled={emailAddress === ""}
+                    type="submit"
+                  >
+                    Let me in!
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent
+                  className={clsx({ hidden: emailAddress !== "" })}
                 >
-                  Let me in!
-                </button>
-              </TooltipTrigger>
-              <TooltipContent className={clsx({ hidden: emailAddress !== "" })}>
-                <p>Enter your email first</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+                  <p>Enter your email first</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="block md:flex w-full md:gap-4 animate-pulse select-none cursor-default">
+          <div className="flex-grow min-w-64">
+            <div className="border rounded-lg h-12 w-full px-4 py-2 bg-slate-300"></div>
+          </div>
+          <div className="flex relative items-center justify-center min-w-44 mt-3 md:mt-0">
+            <div className="h-12 w-full px-4 py-2 md:size-full  rounded-lg font-bold bg-slate-300"></div>
+          </div>
+        </div>
+      )}
     </form>
   );
 };
